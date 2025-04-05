406542255988e730dc40a6a64c7c88b554c2c05c
Julian <julianjometzger@gmail.com>
2025-04-05 10:07:23 +0200

    Now the program can take in parameters

--- lib/read_graph.c
+++ lib/read_graph.c
@@ -1,7 +1,9 @@
 #include "read_graph.h"
 
-void createGraphFile(){
-	system("./jimp2/projekt-4/bin/graphdecoder jimp2/projekt-4/dane/graf.csrrg > output.txt");
+int createGraphFile(char* input_file, char* output_file){
+	char buffer[512];
+	sprintf(buffer, "./jimp2/projekt-4/bin/graphdecoder %s > %s", input_file, output_file);
+	return system(buffer);
 }
 
 int countNodesInFile(FILE* file){
