85da355db15d3e11755f946b094dc8ef6c4c3a40
Julian Metzger <julianjometzger@gmail.com>
2025-04-13 12:14:04 +0200

    udpated the test functionality

--- test.c
+++ test.c
@@ -1,5 +1,5 @@
-#include "lib/utils.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <limits.h>
 #include <math.h>
 #include <string.h>
@@ -23,48 +23,40 @@ int getNextNumberInFile(FILE* file, int *f_number){// f_number - zmienna podana 
 	return 1;
 }
 
-char* getParameter(int param_count, char** params, char* param_prefix){
-	for(int i = 1; i < param_count-1; i++){
-		if(!strcmp(params[i], param_prefix)) return params[i+1];
+int testFile(char* file_name){
+	FILE* file = fopen(file_name, "r");
+	if(!file) return 1;
+
+	int node_count, cluster_count, ideal_cluster_size;
+	double percentage;
+	if(fscanf(file, "nodes:%d clusters:%d percentage:%lf cluster_size:%d\n", &node_count, &cluster_count, &percentage, &ideal_cluster_size) != 4){
+		fprintf(stderr, "Zły format pierwszej linijki w pliku %s", file_name);
+		return 1;
 	}
-	return NULL;
+
+	double minimum_percentage = ((double)(ideal_cluster_size+1) / ideal_cluster_size - 1) * 100;
+	int warning = 0;
+
+	for(int cluster = 0; cluster < cluster_count; cluster++){
+		int current_node_count = 0;
+		int node;
+		while(getNextNumberInFile(file, &node) == 1) current_node_count++;
+
+		double size_difference_percentage = ((double)current_node_count/ideal_cluster_size - 1)*100;
+		if(size_difference_percentage > fmax(percentage, minimum_percentage)) return 2;
+		else if(size_difference_percentage <= minimum_percentage && size_difference_percentage > percentage) warning = 1;
+	}
+
+	fclose(file);
+	return warning;
 }
 
 int main(int argc, char** argv){
 	char* file_name = "clusters.txt";
-	int cluster_count = 2;
-
-	char* new_file_name = getParameter(argc, argv, "-o");
-	char* new_cluster_count = getParameter(argc, argv, "-c");
-	if(new_file_name) file_name = new_file_name;
-	if(new_cluster_count) cluster_count = atoi(new_cluster_count);
-
-	FILE* file = fopen(file_name, "r");
-	if(!file) return 1;
-
-	int node;
-	int node_count = -cluster_count;
-	int max_node_number = -INT_MAX;
-	while(getNextNumberInFile(file, &node)){
-		if(node > max_node_number) max_node_number = node;
-		node_count++;
+	int test_count = 1;
+	for(int i = 0; i < test_count; i++){
+		int test_result = testFile(file_name);
+		char* message = test_result == 2 ? "\x1B[31mFAILURE\x1B[0m" : test_result == 1 ? "\x1B[33mWARNING\x1B[0m" : "\x1B[32mSUCCESS\x1B[0m";
+		printf("%s. %s\n", message, file_name);
 	}
-
-	printf("Wierzchołki w klastrach: %d\nOminięte wierzchołki: %d\n\n", node_count, node_count - max_node_number - 1);
-	
-	printf("Sprawdzenie klastrów:\n");
-	int ideal_cluster_size = node_count/cluster_count;
-	printf("Idealny rozmiar klastra: %d\n\n", ideal_cluster_size);
-	rewind(file);
-	for(int cluster = 0; cluster < cluster_count; cluster++){
-		int current_node_count = 0;
-		while(getNextNumberInFile(file, &node) == 1){
-			if((node_count / cluster_count) < 21)	printf("%d ", node);
-			current_node_count++;
-		}
-		double size_difference_percentage = round(((double)current_node_count/ideal_cluster_size - 1)*100);
-		printf(";\033[1m%d wierzchołków -> zmiana = %.0lf%\033[0m\n", current_node_count, size_difference_percentage);
-	}
-
-	fclose(file);
 }
